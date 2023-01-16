8d4bef3425c4891a9cdc6ea496cf163befb7ca48
Tony <116265620+UkuleleTony@users.noreply.github.com>
2023-01-16 11:37:32 +0000

    Add files via upload

--- CowsAndBulls.py
+++ CowsAndBulls.py
@@ -1,5 +1,5 @@
 #--------------------------------------------------------------------------------------------------
-# Purpose            : This is the classic board game Mastermin
+# Purpose            : This is the classic board game Mastermind
 #                      The computer generates a random 4 digit number and the player has
 #                      to guess the number. After each guess the computer will show how
 #                      many digits of the gues were the right digit in the right place (cows)
@@ -9,6 +9,8 @@
 # Author             : A.S.Harrison
 # Amendment History  : Date         Author          Description
 #                      15Jan2023    A.S.Harrison    Created
+#                      16Jan2023    A.S.Harrison    Added validation for the user's guess
+#                                                   The Bull detection no longer matches multiple instances
 #--------------------------------------------------------------------------------------------------
 import random
 
@@ -24,16 +26,20 @@ intCow = int(0)
 while intCow < 4:                                                       # Keep repeating until the user gets it right
     lstAnswer = list(strAnswer)                                         # Always start with a fresh list of the answer
     intAttempts += 1                                                    # Increment the number of attempts
-    strGuess = input('Enter a 4 digit guess (Q to quit): ')             # Ask the user for a guess
-    if strGuess == 'Q': break                                           # If the user gives up, break outt he loop
-    lstGuess = list(strGuess)                                           # Convert it to a list
+    
+    while True:                                                         # Start the validation loop
+        strGuess = input('Enter a 4 digit guess (Q to quit): ')         # Ask the user for a guess
+        if strGuess == 'Q': break                                       # If the user gives up, break out the loop
+        if strGuess.isnumeric and len(strGuess) == 4: break             # All numeric and a length of 4 is good to go
+        print('Durr! 4 digits all numeric dummy.')                      # Otherwise, the user is an idiot
 
-    # TODO: Validate that we have been given a valid guess (4 digits, all numeric)
+    if strGuess == 'Q': break                                           # If the user wants to quit, jump out of the game loop
 
+    lstGuess = list(strGuess)                                           # Convert the guess to a list (so we can amend the individual digits later)
     intCow = int(0)                                                     # Initialise results
     intBull = int(0)
 
-    for i in range(0,4):                                                # Check for Cows (right number, right place)
+    for i in range(0, 4):                                               # Check for Cows (right number, right place)
         if lstGuess[i] == lstAnswer[i]:                                 # If we find a matching digit (in the right place)
             intCow += 1                                                 # Increment the cow count
             lstGuess[i] = 'x'                                           # Replace the digit, in both places, so that they don't get a 'bull' match
@@ -42,6 +48,7 @@ while intCow < 4:                                                       # Keep r
     for i in range(0,4):                                                # Check for Bulls (right number, wrong place)
         for j in range(0,4):
             if lstGuess[i] == lstAnswer[j]:                             # If we found one
+                lstAnswer[j] = 'z'                                      # Replace the digit so that it isn't matched multiple times
                 intBull += 1                                            # Increment the bull count
 
     print(str(intCow) + ' Cow(s) and ' + str(intBull) + ' Bull(s))')    # Display the hint
